cfb0dc8388632e64cf75b5c81e02ba5e0e64e370
unknown <qiyj6>
2022-10-23 15:22:33 +0800

    test git

---
+++ vitis/test10/test/Debug/src/main.d
@@ -0,0 +1,52 @@
+src/main.o src/main.o: ../src/main.c \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/sleep.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops_hw.h \
+ C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/sleep.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xgpiops_hw.h:
+
+C:/Users/zbl/Desktop/ov5640/vitis/test10/system_wrapper/export/system_wrapper/sw/system_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xplatform_info.h:
